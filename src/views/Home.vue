29909607414c433e915777c4ec7240d0f47958b9
lsc-com <1099421282@qq.com>
2020-04-24 11:58:43 +0800

    home第一次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <span>WeChat</span>
+    </div>
+    <div class="content" >
+        <!-- v-model="" :强制绑定，绑定data中的数据-->
+        <!-- for循环中的item才是当前组件取到的值 -->
+        
+      <mt-cell-swipe
+        :right="[
+          {
+            content: 'Delete',
+            style: { background: 'red', color: '#fff' },
+            handler: () => this.$messagebox('delete')
+          }
+        ]"
+        v-for="item in form" :key="item"
+        >
+        <div class="content_item">
+        <img :src="item.image" alt="" width="30" height="30">
+        <div>
+          {{item.title}}
+          {{item.time}}
+        </div>
+        {{item.content}}
+        </div>
+      </mt-cell-swipe>
+    </div>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="tab1">
+        <img slot="icon" src="../assets/logo.png">
+        tab1
+      </mt-tab-item>
+      <mt-tab-item id="tab2">
+        <img slot="icon" src="">
+        tab2
+      </mt-tab-item>
+      <mt-tab-item id="tab3">
+        <img slot="icon" src="">
+        tab3
+      </mt-tab-item>
+      <mt-tab-item id="tab4">
+        <img slot="icon" src="">
+        tab4
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      form:[
+        {title:'text',content:'content',image:'../assets/logo.png',time:'晚上7:00'},
+        {title:'text1',content:'content1',image:'../assets/logo.png'}
+      ]
+    }
+  },
+  mehtods:{
+
   }
 }
 </script>
+
+<style>
+@import '../../public/css/home.css';
+
+</style>
