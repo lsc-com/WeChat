40922960097b1c8408d33c1ef3c6a6055e298116
yiyidexiaofeng <954869471@qq.com>
2020-04-25 18:20:46 +0800

    微信完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,6 @@
     <div class="content">
       <!-- v-model="" :强制绑定，绑定data中的数据-->
       <!-- for循环中的item才是当前组件取到的值 -->
-<<<<<<< HEAD
-
-=======
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
       <mt-cell-swipe
         :right="[
           {
@@ -29,15 +25,9 @@
           <img :src="item.image" alt width="30" height="30" />
           <div>
             <span class="content_item_title">{{item.title}}</span>
-<<<<<<< HEAD
          <span class="content_item_time" >  {{item.time}}</span>
           </div>
           <span class="content_item_home">{{item.content}}</span>
-=======
-            <span class="content_item_time">{{item.time}}</span>
-          </div>
-          <span class="content_item_content">{{item.content}}</span>
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
         </div>
       </mt-cell-swipe>
     </div>
@@ -45,50 +35,19 @@
     <mt-tabbar>
       <mt-tab-item :id="item.id" v-for="item in list" :key="item.id">
         <span style="display:block">
-<<<<<<< HEAD
           
-=======
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
           <svg class="icon" aria-hidden="true">
             <use :xlink:href="item.icon" />
           </svg>
         </span>
-<<<<<<< HEAD
           <span style="display:block;text-align:center">{{item.name}}</span>
       </mt-tab-item>
     </mt-tabbar>
-=======
-        <span style="display:block;text-align:center">{{item.name}}</span>
-      </mt-tab-item>
-    </mt-tabbar>
-
-    <!-- <mt-tabbar>
-  <mt-tab-item id="tab1">
-    <img slot="icon" src="">
-    tab1
-  </mt-tab-item>
-  <mt-tab-item id="tab2">
-    <img slot="icon" src="">
-    tab2
-  </mt-tab-item>
-  <mt-tab-item id="tab3">
-    <img slot="icon" src="">
-    tab3
-  </mt-tab-item>
-  <mt-tab-item id="tab4">
-    <img slot="icon" src="">
-    tab4
-  </mt-tab-item>
-    </mt-tabbar>-->
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import { Toast } from 'mint-ui';
-=======
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
 export default {
   name: "Home",
   data() {
@@ -99,7 +58,6 @@ export default {
           content: "content",
           image: require("../../public/imgs/touxiang.jpg"),
           time: "晚上7:00"
-<<<<<<< HEAD
         },
         {
           title: "text",
@@ -189,41 +147,20 @@ export default {
       list: [
         { id: "tab1", icon: "#icon-xiaoxi", name: "微信" },
         { id: "tab2", icon: "#icon-tongxunlu-yigai", name: "通讯录" ,tongxunlu:""},
-=======
-        }
-      ],
-      list: [
-        { id: "tab1", icon: "#icon-xiaoxi", name: "微信" },
-        { id: "tab2", icon: "#icon-tongxunlu-yigai", name: "通讯录" },
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
         { id: "tab3", icon: "#icon-faxian3", name: "发现" },
         { id: "tab4", icon: "#icon-wo1", name: "我" }
       ]
     };
   },
-<<<<<<< HEAD
   mehtods: {
     tongxunlu(){
            Toast('登录成功');
             this.$router.push("/Mali list");
         },
   }
-=======
-  mehtods: {}
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
 };
 </script>
 
 <style>
 @import "../../public/css/home.css";
-<<<<<<< HEAD
-=======
-.icon {
-  width: 2em;
-  height: 2em;
-  vertical-align: 0.5em;
-  fill: currentColor;
-  overflow: hidden;
-}
->>>>>>> 522b674fde4f2f437f8a8ea66cfe3b670bd4b690
 </style>
